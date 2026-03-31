826f473dc44bb867044064893e0d7333b10a80cd
James <jamesadler32@gmail.com>
2026-03-31 02:41:08 -0500

    Created Movie, Actor, and MovieActor classes for database

---
+++ Spring2026-Project3-jpadler/Models/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spring2026_Project3_jpadler.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+        
+        public string? Gender { get; set; }
+        
+        public int Age { get; set; }
+
+        public string? IMDBLink { get; set; }
+        
+        public byte[]? Photo { get; set; }
+    }
+}
---
+++ Spring2026-Project3-jpadler/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spring2026_Project3_jpadler.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; } = default!;
+
+        public string? IMDBLink { get; set; }
+
+        public string? Genre { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public byte[]? Poster { get; set; }
+    }
+}
